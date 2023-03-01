cfee4ded86c1325d11a189a2da24a83e271eedbe
Thomas Pierrot <t.pierrot@instadeep.com>
2023-02-28 19:25:46 -0500

    minor fix

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import os
 
 from setuptools import find_packages, setup
 
-from qdax import __version__
-
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(CURRENT_DIR, "README.md"), encoding="utf-8") as f:
@@ -11,20 +9,20 @@ with open(os.path.join(CURRENT_DIR, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="nucleotide_transformer",
-    version=__version__,
+    version="0.0.1",
     packages=find_packages(),
     url="https://github.com/instadeepai/nucleotide-transformer",
     license="CC BY-NC-SA 4.0",
     author="InstaDeep Ltd",
-    description="The Nucleotide Transformer: Building and Evaluating Robust Foundation Models for Human Genomics ",
+    description="The Nucleotide Transformer: Building and Evaluating "
+    "Robust Foundation Models for Human Genomics ",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
         "absl-py>=1.0.0",
         "jax>=0.3.25",
         "jaxlib>=0.3.25",  # necessary to build the doc atm
-        "dm-haiku>=0.0.9"
-        "numpy>=1.23.5",
+        "dm-haiku>=0.0.9" "numpy>=1.23.5",
     ],
     dependency_links=[
         "https://storage.googleapis.com/jax-releases/jax_releases.html",
